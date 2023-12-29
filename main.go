7625fb94c1b6786b9d749d7969dc216b5e7dc919
shubham.patni <shubham.patni@iglooinsure.com>
2023-12-29 19:06:21 +0530

    implemented SVD, toggeled and tested inbuilt JPEG compression and set to best efficiency.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"github.com/labstack/echo/v4"
+	compression "github.com/nurlantulemisov/imagecompression"
 	"image"
 	"image/jpeg"
 	"image/png"
@@ -12,13 +13,6 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
-	//"fmt",
-	//"log",
-	//"encoding/json",
-	//"image/jpeg"
-	//"net/http",
-	//"image/png",
-	//"github.com/gorilla/mux"
 )
 
 func main() {
@@ -28,6 +22,7 @@ func main() {
 	// routes
 	e.POST("compress/jpeg", compressJPEG)
 	e.POST("compress/png", compressPNG)
+	e.POST("compress/SVD", compressSVD)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
@@ -39,6 +34,53 @@ func compressJPEG(c echo.Context) error {
 func compressPNG(c echo.Context) error {
 	return compressImage(c, "png")
 }
+
+func compressSVD(c echo.Context) error {
+	return compressWithSVD(c, "SVD")
+}
+
+func compressWithSVD(c echo.Context, format string) error {
+	// extract image from request
+	file, err := c.FormFile("image")
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	// open file
+	src, err := file.Open()
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "Failed to open the image.")
+	}
+	defer src.Close()
+
+	img, err := jpeg.Decode(src)
+
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	compressing, _ := compression.New(50)
+	compressingImage := compressing.Compress(img)
+
+	f, err := os.Create("/Users/shubhampatni/Desktop/workspace/image_compression/svd-compressed-images/" + file.Filename)
+	if err != nil {
+		log.Fatalf("error creating file: %s", err)
+	}
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
+			log.Fatalf(err.Error())
+		}
+	}(f)
+
+	err = png.Encode(f, compressingImage)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	return c.String(http.StatusOK, "image compressed and stored.")
+}
+
 func compressImage(c echo.Context, format string) error {
 
 	// extract image from request
@@ -70,6 +112,12 @@ func compressImage(c echo.Context, format string) error {
 	case "png":
 		compressedImage, err = encodePNG(img)
 		log.Println("Compressed image is in PNG format.")
+	//case "SVD":
+	//	//compressedImage, err = compressWithSVD(img, 20)
+	//	compressing, _ := compression.New(95)
+	//	compressingImage := compressing.Compress(img)
+	//	log.Println("Compressed image using SVD.")
+
 	default:
 		return c.String(http.StatusBadRequest, "Invalid image format. Supported formats: jpeg, png.")
 	}
@@ -79,8 +127,15 @@ func compressImage(c echo.Context, format string) error {
 	}
 
 	// store compressed image to given directory path
-	outputPath := "/Users/shubhampatni/Desktop/workspace/image_compression/compressed-images/" + file.Filename
+	outputPath := ""
+	//if format == "jpeg" || format == "png" {
+	outputPath = "/Users/shubhampatni/Desktop/workspace/image_compression/compressed-images/" + file.Filename
 	err = storeImage(outputPath, compressedImage)
+	//} else if format == "SVD" {
+	//	outputPath = "/Users/shubhampatni/Desktop/workspace/image_compression/svd-compressed-images/" + file.Filename
+	//	err = storeImage(outputPath, compressingImage)
+	//}
+
 	if err != nil {
 		return c.String(http.StatusInternalServerError, "Failed to store the compressed image.Failed to store the compressed image.")
 	}
@@ -88,10 +143,71 @@ func compressImage(c echo.Context, format string) error {
 	return c.String(http.StatusOK, fmt.Sprintf("Image compressed and stored at %s", outputPath))
 }
 
+//func compressWithSVD(img image.Image, k int) (image.Image, error) {
+//	bounds := img.Bounds()
+//	rows, cols := bounds.Dy(), bounds.Dx()
+//
+//	// convert image to a matrix
+//	matImage := mat.NewDense(rows, cols*3, nil)
+//
+//	for i := 0; i < rows; i++ {
+//		for j := 0; j < cols; j++ {
+//			r, g, b, _ := img.At(j, i).RGBA()
+//			matImage.Set(i, j*3, float64(r>>8))
+//			matImage.Set(i, j*3+1, float64(g>>8))
+//			matImage.Set(i, j*3+2, float64(b>>8))
+//		}
+//	}
+//
+//	// Perform SVD
+//	var u, v mat.Dense
+//	var s mat.VecDense
+//	matImage.SVD(&u, &s, &v)
+//
+//	// Truncate singular values and vectors
+//	truncateSVD(&u, &s, &v, k)
+//
+//	// Reconstruct the compressed matrix
+//	var compressedImage mat.Dense
+//	compressedImage.Mul(&u, v.T())
+//
+//	// Normalize the values in the compressed matrix
+//	compressedImage.Apply(func(_, _ int, v float64) float64 {
+//		return math.Max(0, math.Min(255, v))
+//	}, &compressedImage)
+//
+//	// Convert the matrix back to an image
+//	resultImg := image.NewRGBA(bounds)
+//	for i := 0; i < rows; i++ {
+//		for j := 0; j < cols; j++ {
+//			r := uint8(compressedImage.At(i, j*3))
+//			g := uint8(compressedImage.At(i, j*3+1))
+//			b := uint8(compressedImage.At(i, j*3+2))
+//			resultImg.Set(j, i, color.RGBA{R: r, G: g, B: b, A: 255})
+//		}
+//	}
+//
+//	return resultImg, nil
+//}
+//
+//func truncateSVD(u, s, v *mat.Dense, k int) {
+//	rows, _ := u.Dims()
+//
+//	// Truncate singular values
+//	singularValues := s.RawVector().Data
+//	for i := k; i < len(singularValues); i++ {
+//		singularValues[i] = 0
+//	}
+//
+//	// Truncate rows in U and columns in V
+//	u.Slice(0, rows, 0, k, u)
+//	v.Slice(0, len(singularValues), 0, k, v)
+//}
+
 func encodeJPEG(img image.Image) ([]byte, error) {
 	var buf bytes.Buffer
 	var w = io.Writer(&buf)
-	err := jpeg.Encode(w, img, &jpeg.Options{Quality: 50})
+	err := jpeg.Encode(w, img, &jpeg.Options{Quality: 10})
 	return buf.Bytes(), err
 }
 
---
+++ readme.md
@@ -0,0 +1,24 @@
+<b>Singular Value Decomposition (SVD) in simple terms.<b>
+
+>1. **Start with a Matrix:**
+    - Imagine your data as a big table, like a spreadsheet. Each row could represent an item, and each column could represent a characteristic or feature of that item.
+
+>2. **Decompose the Matrix:**
+    - SVD breaks down this table into three simpler tables. Let's call them U, Σ (Sigma), and V.
+    - U represents how each item (row) in your data is related to certain patterns.
+    - Σ (Sigma) is like a list of strengths. It tells you how important each pattern is.
+    - V represents how each characteristic (column) in your data is related to those patterns.
+
+>3. **Patterns and Strengths:**
+    - U, Σ, and V together help you find important patterns in your data. These patterns are like common themes or trends that run through your items and characteristics.
+    - The strengths in Σ tell you how much each pattern matters. The bigger the strength, the more important that pattern is.
+
+>4. **Simplify and Reconstruct:**
+    - Now, you can decide to keep only the most important patterns and strengths. This is like simplifying your data.
+    - By doing this, you can represent your original data in a simplified way. It's like saying, "I don't need all the details, just show me the most important stuff."
+
+>5. **Useful for Compression:**
+    - Imagine you have a big photo, and each pixel in the photo is like a data point. SVD helps you compress this photo by keeping only the most important patterns and strengths.
+    - Even though you're simplifying, you can still get a pretty good idea of what the original photo looked like.
+
+>In summary, Singular Value Decomposition is like breaking down complex data into simpler patterns and strengths. By focusing on the most important parts, you can understand and represent your data in a more efficient and meaningful way. It's a tool that helps simplify and find the key aspects of your information.
